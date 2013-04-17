b4360008d6e51ad6830ede0466f14ba6d27129ee
George Reese <george.reese@imaginary.com>
2013-04-16 20:51:22 -0500

    Backed out change with hoping for the best

--- src/main/java/org/dasein/cloud/cloudstack/compute/Volumes.java
+++ src/main/java/org/dasein/cloud/cloudstack/compute/Volumes.java
@@ -735,10 +735,10 @@ public class Volumes implements VolumeSupport {
                 else if( name.equals("state") && attribute.hasChildNodes() ) {
                     String state = attribute.getFirstChild().getNodeValue();
 
-                    if( state == null || state.equalsIgnoreCase("allocated") ) {
+                    if( state == null ) {
                         volume.setCurrentState(VolumeState.PENDING);
                     }
-                    else if( state.equalsIgnoreCase("created") || state.equalsIgnoreCase("ready") ) {
+                    else if( state.equalsIgnoreCase("created") || state.equalsIgnoreCase("ready") || state.equalsIgnoreCase("allocated") ) {
                         volume.setCurrentState(VolumeState.AVAILABLE);
                     }
                     else {
